116a06d4e87a9ca32ab9aeac372d2581b7205765
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-01-17 15:29:04 -0500

    imported pages and created routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Workout from './pages/workout';
+import Articles from './pages/articles';
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-    {/* Insert Components Here */}
-    <Navbar />
+    <Navbar /> 
+    <div className='container'>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Workout />} />
+          <Route path="/about" element={<Articles />} />
+      </Routes>
+    </div>
     </div>
   );
 }
