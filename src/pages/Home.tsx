f6223ff9dcf8f7d14285ae69c97b58fa86020507
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-01-31 13:55:02 -0500

    imported home image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/home.css'
+import HomeImage from '../components/banner';
 
 export default function Home() {
     return (
@@ -7,7 +8,7 @@ export default function Home() {
       <div className='cover-photo'>
         {/* cover image goes here */}
       </div>
-
+      <HomeImage />
     </div> 
     )
   
