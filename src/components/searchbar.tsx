24704df6c970977798d2f343999943926ddb6a2a
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-02-02 12:02:26 -0500

    added functions to the search

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-// import { exerciseOptions, fetchData } from '../scripts/fetchData';
+import { exerciseOptions, fetchData } from '../scripts/fetchData';
 import '../styles/searchExercises.css'
 
 const SearchExercises = () => {
+  const [search, setSearch] = useState<string>('')
+
+    
+  const handleSearch = async () => {
+    console.log(search)
+      // if (search)  {
+      //   const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+      // }
+			setSearch('')
+    }
    return (
     <section className='search-container'>
         <div className='heading'>Search for Exercises:</div>
         <section className='text-field-container'>
-           <input type="text"  placeholder="Search Exercises"/>
-            <button>Search</button>
+          <input 
+          type="text" 
+          value = {search} 
+          placeholder="Search Exercises"
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          />
+          <button
+          onClick={handleSearch}
+          >Search</button>
         </section>
     </section>
     )
