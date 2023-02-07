82bf780dce73b832de4f176b54932ed678f0b819
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-02-07 16:23:13 -0500

    removed unused code

--- client/src/components/loader.tsx
+++ client/src/components/loader.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import { InfinitySpin } from 'react-loader-spinner';
 import '../styles/loader.css'
 
 const Loader = () => (
-    // <section className="loaderContainer">
-    //   <InfinitySpin color="grey" />
-    // </section>
     <div className="loading-container">
       <div className="loading-spinner">
       </div>
